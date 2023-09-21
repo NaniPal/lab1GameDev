5efc05ab658168450e72afb0a03a826288f228e4
NaniPal <yeskendirov.dias@gmail.com>
2023-09-21 21:44:46 +0600

    first commit

---
+++ Assets/Scenes/InteractionController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class InteractionController : MonoBehaviour
+ {
+ public GameObject objectToChangeColor;
+ private Renderer objectRenderer;
+ void Start()
+ {
+ objectRenderer = objectToChangeColor.GetComponent<Renderer>();
+ }
+ void Update()
+ {
+ if (Input.GetMouseButtonDown(0))
+ {
+
+ objectRenderer.material.color = Color.red;
+ }
+ else if (Input.GetMouseButtonDown(1)) 
+ {
+
+ objectRenderer.material.color = Color.blue;
+ }
+ }
+ }
